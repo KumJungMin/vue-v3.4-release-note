b02bd741933b205c23fbef6da645baed3da99417
KumJungMin <rmawjdals@naver.com>
2024-02-26 21:37:29 +0900

    feat: once watcher prev, new example

---
+++ src/examples/once-watcher/NewOnceWatcher.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const msg = ref("Hello World!");
+const result1 = ref("");
+const result2 = ref("");
+
+watch(msg, (v) => {
+  result1.value = v;
+});
+
+watch(
+  msg,
+  (v) => {
+    result2.value = v;
+  },
+  {
+    once: true,
+  }
+);
+</script>
+
+<template>
+  <input v-model="msg" />
+  <div>입력값: {{ result1 }}</div>
+  <div>입력값: {{ result2 }}</div>
+</template>
---
+++ src/examples/once-watcher/PrevOnceWatcher.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const msg = ref("Hello World!");
+const result1 = ref("");
+const result2 = ref("");
+
+const unwatcher = watch(msg, (v) => {
+  result2.value = v;
+  unwatcher();
+});
+</script>
+
+<template>
+  <h3>Once Watcher, unwatcher 방식</h3>
+  msg: <input v-model="msg" />
+  <div>입력값1: {{ result1 }}</div>
+  <div>입력값2(once): {{ result2 }}</div>
+</template>
