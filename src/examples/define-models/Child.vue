52f0892dbf2d5b45c95d24b913dcbb29377b995a
KumJungMin <rmawjdals@naver.com>
2024-02-26 21:37:12 +0900

    feat: defineModel example

---
+++ src/examples/define-models/Child.vue
@@ -0,0 +1,8 @@
+<script setup>
+const count = defineModel("count", { type: Number, default: 0 });
+</script>
+
+<template>
+  <button @click="count++">증가</button>
+  {{ count }}
+</template>
---
+++ src/examples/define-models/Index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import Child from "./Child.vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <Child :count />
+</template>
